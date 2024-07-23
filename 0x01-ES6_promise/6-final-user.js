71916fbe3bfe8bf32854d3623436608f5f2ba6e6
thatguykwameC <thatguykwamec@gmail.com>
2024-07-23 16:17:08 -0400

    Handles multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+/**
+ * Handles the profile signup process.
+ *
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The name of the file.
+ * @return {Promise<Array<Object>>} A promise that resolves with an array
+ * of objects containing the status and value or error of each promise.
+ */
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const handlePromise = (promise) => promise.then((result) => ({ status: 'fulfilled', value: result })).catch((error) => ({ status: 'rejected', error: `Error: ${error.message}` }));
+
+  const promises = [handlePromise(signUpUser(firstName, lastName)), handlePromise(uploadPhoto(fileName))];
+
+  return Promise.all(promises);
+}
