92f8d44ba26703a1fa7a38a484422222182614de
thatguykwameC <thatguykwamec@gmail.com>
2024-07-24 21:05:17 -0400

    Edits to docstrimg

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,31 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 /**
- * Handles the profile signup process.
+ * Handles the profile signup process asynchronously.
  *
  * @param {string} firstName - The first name of the user.
  * @param {string} lastName - The last name of the user.
  * @param {string} fileName - The name of the file.
  * @return {Promise<Array<Object>>} A promise that resolves with an array
- * of objects containing the status and value or error of each promise.
+ * of objects containing the status, value or error of each promise.
  */
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
-  const handlePromise = (promise) =>
-    promise
-      .then((result) => ({ status: 'fulfilled', value: result }))
-      .catch((error) => ({
-        status: 'rejected',
-        error: `Error: ${error.message}`,
-      }));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 
-  const promises = [
-    handlePromise(signUpUser(firstName, lastName)),
-    handlePromise(uploadPhoto(fileName)),
-  ];
-
-  return Promise.all(promises);
+  return results.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  }));
 }
