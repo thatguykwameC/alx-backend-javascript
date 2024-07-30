98f9c9533ab25fc9fbaa268cde2221588f0f3a88
thatguykwameC <thatguykwamec@gmail.com>
2024-07-30 04:55:01 -0400

    Funtion that returns an array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of student objects with their
+ * id, first name, and location.
+ *
+ * @return {Array<Object>} An array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
