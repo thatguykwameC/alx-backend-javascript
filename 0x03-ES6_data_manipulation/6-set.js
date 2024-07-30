1a81ec70322080ba7501ba5323fae7ab476d28bd
thatguykwameC <thatguykwamec@gmail.com>
2024-07-30 05:53:58 -0400

    A function that converts an array into a set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a new Set from the given array.
+ *
+ * @param {Array} array - The array to convert into a Set.
+ * @return {Set} A new Set object containing the elements from the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
