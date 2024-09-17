e341ece633caf56cd68011303b5dd4015649a129
thatguykwameC <thatguykwamec@gmail.com>
2024-09-17 19:30:44 -0400

    prints a message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+/**
+ * Prints a given message to the console.
+ *
+ * @param {string} message - The message to be printed
+ * to the console.
+ * @return {undefined} No return value.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
