7d6971265525d69c3494cf20ebf56635f812d65c
thatguykwameC <thatguykwamec@gmail.com>
2024-09-17 20:10:05 -0400

    Creates a complex server

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const http = require('http');
+const displayMessage = require('./0-console');
+
+const HOST = 'localhost';
+const PORT = 1245;
+const stFile = require('./file_handler');
+
+const argPath = process.argv.length > 2 ? process.argv[2] : '';
+
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  switch (req.url) {
+    case '/':
+      res.statusCode = 200;
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      if (!argPath) {
+        res.statusCode = 400;
+        res.end('Missing Arg! Argument count should be 2');
+      } else {
+        try {
+          const result = await stFile(argPath);
+          res.statusCode = 200;
+          res.end(result);
+        } catch (err) {
+          res.statusCode = 500;
+          res.end('Cannot load the database');
+        }
+      }
+      break;
+    default:
+      res.statusCode = 404;
+      res.end('Not Found');
+      break;
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  displayMessage(`Server listening to http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
