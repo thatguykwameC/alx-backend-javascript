4f463061a99c66ff38be46d94b39f9626587bfb4
thatguykwameC <thatguykwamec@gmail.com>
2024-09-19 13:15:16 -0400

    Fixed issues

--- 0x05-Node_JS_basic/handler.js
+++ 0x05-Node_JS_basic/handler.js
@@ -3,34 +3,43 @@
  */
 const fs = require('fs').promises;
 
-function getStudentCountByField(path) {
+function stFile(path) {
   return fs
-    .readFile(path, 'utf-8')
-    .then((data) => {
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const [, ...students] = lines;
-      const studentCountsByField = {};
+    .stat(path)
+    .then((stats) => {
+      if (!stats.isFile()) {
+        return Promise.reject(new Error('Cannot load the database'));
+      }
+      return fs.readFile(path, 'utf-8');
+    })
+    .then((dt) => {
+      const ln = dt.split('\n').filter((ln) => ln.trim() !== '');
+
+      const students = ln.slice(1);
+      let strContainer = `Number of students: ${students.length}`;
+
+      const fields = {};
 
       students
-        .map((student) => {
-          const [firstName, , , field] = student.split(',');
-          return { firstName, field };
+        .map((std) => {
+          const [firstname, , , field] = std.split(',');
+          return { firstname, field };
         })
-        .filter(({ firstName, field }) => firstName && field)
-        .forEach(({ firstName, field }) => {
-          studentCountsByField[field] = (
-            studentCountsByField[field] || []
-          ).concat(firstName);
+        .filter(({ firstname, field }) => firstname && field)
+        .forEach(({ firstname, field }) => {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
         });
 
-      return `This is the list of our students\nNumber of students: ${
-        students.length
-      }${Object.entries(studentCountsByField).reduce(
-        (str, [field, names]) => `${str}\nNumber of students in ${field}: ${
+      for (const [field, names] of Object.entries(fields)) {
+        strContainer += `\nNumber of students in ${field}: ${
           names.length
-        }. List: ${names.join(', ')}`,
-        '',
-      )}`;
+        }. List: ${names.join(', ')}`;
+      }
+
+      return `This is the list of our students\n${strContainer}`;
     })
     .catch((error) => Promise.reject(error));
 }
